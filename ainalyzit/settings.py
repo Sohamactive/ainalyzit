a6538b3ab24bd5c8a9c7fbd71016fa4232e310fd
Soham Sharma <sharmasoham1427@gmail.com>
2025-07-27 19:43:11 +0530

    Update settings.py

--- ainalyzit/settings.py
+++ ainalyzit/settings.py
@@ -95,6 +95,7 @@ USE_TZ = True
 # Static and Media files
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
